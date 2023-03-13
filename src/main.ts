aa5ee0b4df1639d75a93426c419c3c1b81e6c162
Rowan Stroo Cloeck <rowanboris@hotmail.com>
2023-03-13 15:04:42 +0100

    cookie banner added

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,10 @@ import '@quasar/extras/fontawesome-v6/fontawesome-v6.css'
 // Import Quasar css
 import 'quasar/dist/quasar.css'
 
+import VueCookieAcceptDecline from 'vue-cookie-accept-decline';
+import 'vue-cookie-accept-decline/dist/vue-cookie-accept-decline.css';
+
+
 import App from './App.vue'
 
 import './assets/style/main.css'
@@ -18,6 +22,7 @@ app.use(Quasar, {
     plugins: {}, // import Quasar plugins and add here
 })
 
+app.component('vue-cookie-accept-decline', VueCookieAcceptDecline);
 app.config.globalProperties.locale = "en-GB";
 
 app.mount('#app');
